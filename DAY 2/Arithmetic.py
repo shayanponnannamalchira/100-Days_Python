0300fd6c4f6fcbfdf00b7a58d88bdb15e0adcef0
shayanponnannamalchira <shayan.malchira@s.amity.edu>
2025-09-29 22:39:40 +0530

    DAY 2

---
+++ DAY 2/Arithmetic.py
@@ -0,0 +1,18 @@
+num_1=int(input("Enter first number: "))
+num_2=int(input("Enter second number: "))
+
+# Arithmetic Operations
+sum=num_1 + num_2
+difference=num_1 - num_2
+product=num_1 * num_2
+quotient=num_1 / num_2
+floor_division=num_1 // num_2
+modulus=num_1 % num_2
+exponentiation=num_1 ** num_2
+print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
+print("Floor Division:", floor_division)
+print("Modulus:", modulus)
+print("Exponentiation:", exponentiation)
