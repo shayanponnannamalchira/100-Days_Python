1b43e840db93524f375265cb8f5b2adaf2d5c104
Shayan Ponnanna Malchira <shayan.malchira@s.amity.edu>
2025-10-28 21:01:01 +0530

    DAY 11

---
+++ Day 11/calculator.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+print("Enter two numbers:")
+a=int(input())
+b=int(input())
+print("Sum =", add(a,b))
+print("Difference =", subtract(a,b))
+print("Product =", multiply(a,b))
